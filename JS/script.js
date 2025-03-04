1f559b53842263fadb5307620e08e3efc2b6994f
Valeriya <leradenisenko.9@gmail.com>
2025-03-04 15:30:18 +0600

    +js файл

---
+++ JS/script.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const modalTitle = document.querySelector('form__title');
+const modalForm = document.querySelector('.form');
+const modalCheckbox = document.querySelector('.form__checkbox');
+const modalCheckboxInput = document.querySelector('.form__discount-input');
+const totalPrice = document.querySelector('.form__total_price');
