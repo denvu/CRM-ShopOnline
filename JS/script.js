31537c73e1985b7875f0cd812d7fcddcbbcc9fdb
Valeriya <leradenisenko.9@gmail.com>
2025-03-05 21:27:45 +0600

    + ID и кнопка закрытия

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const modalTitle = document.querySelector('form__title');
+const modalIdProduct = document.querySelector('.form__ID-product');
+const modalBtnClose = document.querySelector('.form__close');
 const modalForm = document.querySelector('.form');
 const modalCheckbox = document.querySelector('.form__checkbox');
 const modalCheckboxInput = document.querySelector('.form__discount-input');
